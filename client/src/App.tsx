927bdd6c46b9a006b3b6d7e21b330e5543905f8e
Kobgones <morgan.mezaache.pro@gmail.com>
2023-10-03 15:51:57 +0200

    Edited app.tsx 6

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -32,7 +32,7 @@ function App() {
 			<Header>
 				<Container>
 					<h1>Wilders Book with CD</h1>
-					<h2>MARION lala</h2>
+					<h2>MARION lili</h2>
 				</Container>
 			</Header>
 			<Container>
