c49b46e31f666dbde1a46d05719bea9a17410d9c
Kobgones <morgan.mezaache.pro@gmail.com>
2023-10-03 14:37:09 +0200

    Added docker compose prod yml and edited app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -32,7 +32,7 @@ function App() {
 			<Header>
 				<Container>
 					<h1>Wilders Book with CD</h1>
-					<h2>COUCOU</h2>
+					<h2>ET COUCOU TOI</h2>
 				</Container>
 			</Header>
 			<Container>
